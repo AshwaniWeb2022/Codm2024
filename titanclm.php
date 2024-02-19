a0ef65814ec972d6b8a70dbd3d263e4ecc2c3e18
hardik <hardiksharma7919@gmail.com>
2024-02-19 20:21:54 +0530

    titan clm page

---
+++ titanclm.php
@@ -0,0 +1,241 @@
+<?php include 'app/header.php';?>
+
+<style>
+    
+.T-form-tital {
+    font-size: 56px;
+    font-weight: 700;
+    line-height: 60px
+}
+
+.T-form-subTital {
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 30px;
+    color: #626464
+}
+
+.T-F-no_cod_tital {
+    font-size: 30px;
+    font-weight: 600
+}
+
+.T-F-no_cod_subtital {
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 30px;
+    color: #626464
+}
+
+.F-T-margin-top {
+    margin-top: 8%
+}
+
+.T-bg-color {
+    padding: 8px;
+    border-radius: 5px;
+    background-color: aqua;
+    color: #ffffff
+}
+
+.S-A-full_page_product {
+    position: relative;
+    display: block;
+    padding: 10px 10px 10px 40px;
+    background: #fff;
+    color: #000;
+    text-decoration: none;
+    box-shadow: inset .4em 0 #29ade4;
+    -webkit-transition: box-shadow 1s;
+    transition: box-shadow 1s;
+    border: 1px solid #29ade4;
+    font-size: 20px;
+    font-weight: 500;
+    border-radius: 0 5px 5px 0;
+    text-transform: uppercase
+}
+
+.S-A-full_page_product:hover {
+    box-shadow: inset 1.5em 0 #29ade4
+}
+</style>
+
+<div data-elementor-type="wp-page" data-elementor-id="61" class="elementor elementor-61">
+	<div class="elementor-element elementor-element-7cf7f80e e-con-boxed e-con" data-id="7cf7f80e" data-element_type="container" data-settings="{&quot;content_width&quot;:&quot;boxed&quot;}">
+		<div class="e-con-inner">
+			<div class="elementor-element elementor-element-235a08d4 elementor-invisible elementor-widget elementor-widget-heading" data-id="235a08d4" data-element_type="widget" data-settings="{&quot;_animation&quot;:&quot;fadeIn&quot;}" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+					<style>
+						.elementor-heading-title {
+							padding: 0;
+							margin: 0;
+							line-height: 1
+						}
+
+						.elementor-widget-heading .elementor-heading-title[class*=elementor-size-]>a {
+							color: inherit;
+							font-size: inherit;
+							line-height: inherit
+						}
+
+						.elementor-widget-heading .elementor-heading-title.elementor-size-small {
+							font-size: 15px
+						}
+
+						.elementor-widget-heading .elementor-heading-title.elementor-size-medium {
+							font-size: 19px
+						}
+
+						.elementor-widget-heading .elementor-heading-title.elementor-size-large {
+							font-size: 29px
+						}
+
+						.elementor-widget-heading .elementor-heading-title.elementor-size-xl {
+							font-size: 39px
+						}
+
+						.elementor-widget-heading .elementor-heading-title.elementor-size-xxl {
+							font-size: 59px
+						}
+					</style>
+					<h1 class="elementor-heading-title elementor-size-default">We are Partner of TITAN</h1>
+				</div>
+			</div>
+			<div class="elementor-element elementor-element-61c29187 elementor-widget elementor-widget-heading" data-id="61c29187" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+					<h5 class="elementor-heading-title elementor-size-default">Streamline CLM with Easy Contract Lifecycle Management Solutions</h5>
+				</div>
+			</div>
+		</div>
+	</div>
+<section class="">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mx-auto align-self-center order-md-2 p-md-5 mb-2">
+                <div class="T-form-tital">A powerful custom CLM Application for Salesforce</div>
+                <div class="T-form-subTital pt-4">Create automated and connected contract lifecycle processes for Salesforce using Titan’s zero code CLM Application</div>
+                <div class="mt-4"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Build</span></div>
+                <div class="mt-3"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Collaborate</span></div>
+                <div class="mt-3"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Sign</span></div>
+            </div>
+            <div class="col-lg-5 mx-auto align-self-center mb-2">
+                <img src="wp-content/uploads/sites/231/2022/Products/CLM-1.png" alt="form-2" class="w-100">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="wp-content/uploads/\sites/231/2022/Products/icon-1-1-1.svg" alt="">
+                <div class="T-F-no_cod_tital pt-2">360 Degree Tracking</div>
+                <div class="pt-3 T-F-no_cod_subtital">Fully track your document in Salesforce from start to finish</div>
+            </div>
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="wp-content/uploads/sites/231/2022/Products/icon-2-1-1.svg" alt="">
+                <div class="T-F-no_cod_tital pt-3">Dynamically Generated Docs</div>
+                <div class="pt-3 T-F-no_cod_subtital">All documents are dynamically generated through live push and pull of Salesforce data</div>
+            </div>
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="wp-content/uploads/sites/231/2022/Products/icon-4-2.svg" alt="">
+                <div class="T-F-no_cod_tital pt-2">Fully Collaborative and Flexible</div>
+                <div class="pt-3 T-F-no_cod_subtital">Collaboration is fully flexible, and you can add participants at any stage of the CLM journey</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-6 mx-auto align-self-center  p-md-5">
+                <div class="T-form-tital align-self-center">Create seamless contract lifecycle processes</div>
+                <div class="T-form-subTital pt-4">Build flexible and integrated contract lifecycle experiences, bringing together users both in and outside of Salesforce</div>
+            </div>
+                    <div class="col-lg-6 mx-auto">
+                <img src="wp-content/uploads/sites/231/2022/Products/CLM-2.png" alt="form-1" class="w-100">
+            </div>
+            </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-6 mx-auto align-self-center order-md-2 p-md-5 mb-2 p-md-5">
+                <div class="T-form-tital align-self-center">Manage and track document versions in Salesforce</div>
+                <div class="T-form-subTital pt-4">Manage, review, and track documents directly within Salesforce for a 360 degree view of the contract lifecycle</div>
+            </div>
+                    <div class="col-lg-5 mx-auto">
+                <img src="wp-content/uploads/sites/231/2022/Products/CLM-3.png" alt="form-1" class="w-100">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-6 mx-auto align-self-center  p-md-5">
+                <div class="T-form-tital align-self-center">Collaborate dynamically</div>
+                <div class="T-form-subTital pt-4">Add collaborators at any stage of the contract lifecycle</div>
+            </div>
+                    <div class="col-lg-5 mx-auto">
+                <img src="wp-content/uploads/sites/231/2022/Products/CLM-4.png" alt="form-1" class="w-100">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-6 mx-auto align-self-center order-md-2 p-md-5 mb-2  p-md-5">
+                <div class="T-form-tital align-self-center">Get dynamic contract templates</div>
+                <div class="T-form-subTital pt-4">Get dynamic contract templates, pre-filled with live Salesforce data</div>
+                <div class="mt-4"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Salesforce Automation</span></div>
+                <div class="mt-4"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Multiple Signers</span></div>
+                <div class="mt-4"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Efficient Contract Lifecycles</span></div>
+                <div class="mt-4"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Integrated Salesforce Experiences</span></div>
+            </div>
+                    <div class="col-lg-5 mx-auto">
+                <img src="wp-content/uploads/sites/231/2022/Products/CLM-5.png" alt="form-1" class="w-100">
+            </div>
+           
+        </div>
+    </div>
+</section>
+
+
+<section class="mt-5">
+    <div class="container">
+    <h2 class="mb-3 sub_titel  text-center"> Titan CLM Features</h2>
+        <div class="row pt-4">
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Collaborate Online and Locally</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Multiple Signers</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Add rounds of Collaborations</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Dynamic Content</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Codeless Contract Approvals</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Automated Mail Approvals</div>
+            </div>
+        </div>
+    </div>
+</section>
+<hr>
+
+
+
+<?php include 'app/footer.php';?>
