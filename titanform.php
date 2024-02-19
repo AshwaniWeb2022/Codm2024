7d3fcc8b11fe1ed2c0bda86e61778cabd1a85728
hardik <hardiksharma7919@gmail.com>
2024-02-19 20:25:47 +0530

    titanform

---
+++ titanform.php
@@ -0,0 +1,253 @@
+<?php include 'app/header.php';?>
+<style>
+    
+.T-form-tital {
+    font-size: 56px;
+    font-weight: 700;
+    line-height: 60px
+}
+
+.T-form-subTital {
+    font-size: 20px;
+    font-weight: 400;
+    line-height: 30px;
+    color: #626464
+}
+
+.T-F-no_cod_tital {
+    font-size: 30px;
+    font-weight: 600
+}
+
+.T-F-no_cod_subtital {
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 30px;
+    color: #626464
+}
+
+.F-T-margin-top {
+    margin-top: 8%
+}
+
+.T-bg-color {
+    padding: 8px;
+    border-radius: 5px;
+    background-color: aqua;
+    color: #ffffff
+}
+
+.S-A-full_page_product {
+    position: relative;
+    display: block;
+    padding: 10px 10px 10px 40px;
+    background: #fff;
+    color: #000;
+    text-decoration: none;
+    box-shadow: inset .4em 0 #29ade4;
+    -webkit-transition: box-shadow 1s;
+    transition: box-shadow 1s;
+    border: 1px solid #29ade4;
+    font-size: 20px;
+    font-weight: 500;
+    border-radius: 0 5px 5px 0;
+    text-transform: uppercase
+}
+
+.S-A-full_page_product:hover {
+    box-shadow: inset 1.5em 0 #29ade4
+}
+</style>
+
+<div data-elementor-type="wp-page" data-elementor-id="61" class="elementor elementor-61">
+	<div class="elementor-element elementor-element-7cf7f80e e-con-boxed e-con" data-id="7cf7f80e" data-element_type="container" data-settings="{&quot;content_width&quot;:&quot;boxed&quot;}">
+		<div class="e-con-inner">
+			<div class="elementor-element elementor-element-235a08d4 elementor-invisible elementor-widget elementor-widget-heading" data-id="235a08d4" data-element_type="widget" data-settings="{&quot;_animation&quot;:&quot;fadeIn&quot;}" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+					<style>
+						.elementor-heading-title {
+							padding: 0;
+							margin: 0;
+							line-height: 1
+						}
+
+						.elementor-widget-heading .elementor-heading-title[class*=elementor-size-]>a {
+							color: inherit;
+							font-size: inherit;
+							line-height: inherit
+						}
+
+						.elementor-widget-heading .elementor-heading-title.elementor-size-small {
+							font-size: 15px
+						}
+
+						.elementor-widget-heading .elementor-heading-title.elementor-size-medium {
+							font-size: 19px
+						}
+
+						.elementor-widget-heading .elementor-heading-title.elementor-size-large {
+							font-size: 29px
+						}
+
+						.elementor-widget-heading .elementor-heading-title.elementor-size-xl {
+							font-size: 39px
+						}
+
+						.elementor-widget-heading .elementor-heading-title.elementor-size-xxl {
+							font-size: 59px
+						}
+					</style>
+					<h1 class="elementor-heading-title elementor-size-default">We are Partner of TITAN</h1>
+				</div>
+			</div>
+			<div class="elementor-element elementor-element-61c29187 elementor-widget elementor-widget-heading" data-id="61c29187" data-element_type="widget" data-widget_type="heading.default">
+				<div class="elementor-widget-container">
+					<h5 class="elementor-heading-title elementor-size-default">Streamline CLM with Easy Contract Lifecycle Management Solutions</h5>
+				</div>
+			</div>
+		</div>
+	</div>
+<section class="">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-6 mx-auto p-md-5 mb-2">
+                <div class="T-form-tital">Salesforce Forms for any Use Case and Object</div>
+                <div class="T-form-subTital pt-4">Titan's Salesforce Forms reate custom forms, collect data, and automate processes with our drag-and-drop form builder. No coding required, Bi-Salesforce Integration with any object</div>
+            </div>
+            <div class="col-lg-5 mx-auto mb-2">
+                <img src="wp-content/uploads/sites/231/2022/Products/titan_form_1.png" alt="form-1" class="w-100">
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-6 mx-auto align-self-center order-md-2 p-md-5">
+                <div class="T-form-tital">Get powerful & customized Salesforce Forms</div>
+                <div class="T-form-subTital pt-4">Titan's Salesforce Forms reate custom forms, collect data, and automate processes with our drag-and-drop form builder. No coding required, Bi-Salesforce Integration with any object</div>
+                <div class="mt-4"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Build</span></div>
+                <div class="mt-3"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Deploy</span></div>
+                <div class="mt-3"><span><i class="fa-solid fa-check T-bg-color"></i></span> <span class="fw-bold">Integrate</span></div>
+            </div>
+            <div class="col-lg-5 mx-auto align-self-center">
+                <img src="wp-content/uploads/sites/231/2022/Products/titan_form_2.png" alt="form-2" class="w-100">
+            </div>
+        </div>
+    </div>
+</section> 
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row text-center">
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="wp-content/uploads/sites/231/2022/Products/icon-1-1-1.svg" alt="form icon">
+                <div class="T-F-no_cod_tital pt-2">No-code Design</div>
+                <div class="pt-3 T-F-no_cod_subtital">Get Salesforce forms and custom web portals with Titan’s simple drag and drop builder.</div>
+            </div>
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="wp-content/uploads/sites/231/2022/Products/icon-2-1-1.svg" alt="form icon">
+                <div class="T-F-no_cod_tital pt-3">Regulatory Compliant</div>
+                <div class="pt-3 T-F-no_cod_subtital">Ensure ultimate compliance with regulatory frameworks, including HIPAA and GDPR.</div>
+            </div>
+            <div class="col-lg-4 mx-auto mb-3">
+                <img src="wp-content/uploads/sites/231/2022/Products/icon-4-2.svg" alt="form icon">
+                <div class="T-F-no_cod_tital pt-2">End-to-end Secure</div>
+                <div class="pt-3 T-F-no_cod_subtital">Secure sensitive organizational data with SalesForce Shield’s end-to-end encryption.</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mx-auto align-self-center order-md-2 p-md-5">
+                <div class="T-form-tital">Easily embed forms using Script or iFrame</div>
+                <div class="T-form-subTital pt-5">Publish and embed fully responsive dynamic forms for Salesforce directly to a web page, form, or document using both Script and iFrame.</div>
+            </div>
+            <div class="col-lg-5 mx-auto align-self-center ">
+                <img src="wp-content/uploads/sites/231/2022/Products/titan_form-3.png" alt="form-3" class="w-100">
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section class="F-T-margin-top">
+    <div class="container">
+        <div class="row ">
+            <div class="col-lg-6 mx-auto align-self-center p-md-5 mb-2">
+                <div class="T-form-tital ">Push and pull real-time Salesforce data</div>
+                <div class="T-form-subTital pt-5">Share bi-directional documents with the ability to push and pull real-time data directly from your team’s Salesforce databases.</div>
+            </div>
+            <div class="col-lg-5 mx-auto align-self-center mb-2">
+                <img src="wp-content/uploads/sites/231/2022/Products/titan_form-4.png" alt="form-4" class="w-100">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mx-auto  align-self-center order-md-2 p-md-5">
+                <div class="T-form-tital ">Automate repetitive tasks and templates</div>
+                <div class="T-form-subTital pt-5">Easily create digital work processes between customers, partners, and employees in order to do more in less time.</div>
+            </div>
+            <div class="col-lg-5 mx-auto align-self-center">
+                <img src="wp-content/uploads/sites/231/2022/Products/titan_form_5.png" alt="form-5" class="w-100">
+            </div>
+
+        </div>
+    </div>
+</section>
+
+
+<section class="mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 mx-auto align-self-center p-md-5 mb-2">
+                <div class="T-form-tital ">Built for real life scenarios</div>
+                <div class="T-form-subTital pt-5">Not like other form tools for Salesforce, our product is very flexible, it allows you to read and write from multiple objects in real time.</div>
+                <div class="T-form-subTital pt-4">You will be able to add any logic and also to work with any data structure: 1 to many, many to many, junction objects, lookup fields, formulas, list views, and related lists.</div>
+                <div class="T-form-subTital pt-4">Titan is The only product that enables you to edit bulk records, grid views and upload files, and to do whatever you want to do without any code.</div>
+            </div>
+            <div class="col-lg-5 mx-auto align-self-center">
+                <img src="wp-content/uploads/sites/231/2022/Products/titan_form_6.png" alt="form-6" class="w-100">
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mt-5">
+    <div class="container">
+    <h2 class="mb-3 sub_titel  text-center"> Titan Forms Features</h2>
+        <div class="row pt-4">
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Conditional Logic</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Payment Integrations</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Maps & Schedulers</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Responsive Design</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Dynamic Content</div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+                <div class="S-A-full_page_product">Advanced Reports</div>
+            </div>
+        </div>
+    </div>
+</section> 
+<hr>
+
+
+<?php include 'app/footer.php';?>
